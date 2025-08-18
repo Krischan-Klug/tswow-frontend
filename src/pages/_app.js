9f8909700771a2febab4a02cfb24681e1436a613
Krischan-Klug <krischanklug@icloud.com>
2025-08-19 01:40:03 +0200

    quick dirty hydration fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import { use, useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  });
+
+  if (!loaded) {
+    return <p>Loading...</p>;
+  }
+  if (loaded) {
+    return <Component {...pageProps} />;
+  }
 }
